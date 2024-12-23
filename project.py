39585a7120daa652bf31c13cc2e471b12e97e000
Mekaela <mekaelas@hotmail.com>
2024-12-23 17:32:43 +0000

    Added required files for assignment

---
+++ project.py
@@ -0,0 +1,16 @@
+# required for project to be at root level
+''' Your 3 required custom functions other than main must also be in 
+project.py and defined at the same indentation level as main 
+(i.e., not nested under any classes or functions).'''
+
+''' e.g.:
+def main():
+...
+def function_1():
+...
+def function_2():
+...
+def function_n():
+...
+if __name__ == "__main__":
+main() '''
