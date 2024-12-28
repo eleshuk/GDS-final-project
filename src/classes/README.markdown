05d84bcb260b6714d4a6fdf58d06bccbeac63fe8
Elizabeth Leshuk <eleshuk@gmail.com>
2024-12-28 16:28:18 +0000

    Remove cached references to large files

--- src/classes/README.markdown
+++
@@ -1,1 +0,0 @@
-This is where the backend code goes. Create a folder each time you are creating a new functionality
--- src/shared.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-import pandas as pd
-
-# not currently doing anything. Remove if not needed
-app_dir = Path(__file__).parent
